ac635f3c2cc57d652bb2d9f71c177d42d4de465a
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-03-07 22:39:15 +0000

    Still working

--- flake.nix
+++ flake.nix
@@ -19,13 +19,9 @@
         {
           devShells.default = mkShell {
             buildInputs = [
-     git gitRepo gnupg autoconf curl
-     procps gnumake util-linux m4 gperf unzip
      cudatoolkit linuxPackages.nvidia_x11
      libGLU libGL
      xorg.libXi xorg.libXmu freeglut
-     xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib
-     ncurses5 stdenv.cc binutils
      pkgs-stable.julia
                           ];
     CUDA_PATH=pkgs.cudatoolkit;
